9462e70697c3fd8258e725bd178008679bf0ecf4
NadaHe <dodyhesham2001@gmail.com>
2023-07-25 14:21:10 +0300

    optimized code

--- ES.Next/Task2/scripts/task2.js
+++ ES.Next/Task2/scripts/task2.js
@@ -16,36 +16,48 @@ function getArtists(){
          if(xhr.readyState == 4){
              if(xhr.status == 200){
                 var ArtistList = document.getElementById("Artist");
+                
+                ArtistList.innerHTML = ""; 
+                for (let i in JSON.parse(xhr.responseText)){
+                    if(i==selectedBand)
+                    {
+                        console.log(i);
+                        band= JSON.parse(xhr.responseText)[i]; //subscribtor notation
+                        console.log(band);
+                        for(let s = 0 ; s<band.length ; s++){
+                            ArtistList.innerHTML += "<option>"+band[s].name+"</option>"
+                        }
+                    }
+                }
+                // if(selectedBand=="beatles"){
+                //     // console.log(JSON.parse(xhr.responseText)); //[{},{},{},{}]
+                //     band= JSON.parse(xhr.responseText).beatles;
+                //     for(let i = 0; i<band.length;i++){
+                //         ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
+                //     }
+                // }
+                // else if(selectedBand=="stones"){
+                //     // console.log( JSON.parse(xhr.responseText).stones);  //[{},{},{},{}]
+                //     band= JSON.parse(xhr.responseText).stones;
+                //     for(let i = 0; i<band.length;i++){
+                //         ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
+                //     }
+                // }
+                // else if(selectedBand=="genesis"){
+                //     // console.log( JSON.parse(xhr.responseText).genesis);  //[{},{},{},{}]
+                //     band= JSON.parse(xhr.responseText).genesis;
+                //     for(let i = 0; i<band.length;i++){
+                //         ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
+                //     }
+                // }
 
-                if(selectedBand=="beatles"){
-                    // console.log( xhr.responseText.substring(5,12)); //[{},{},{},{}]
-                    band= JSON.parse(xhr.responseText).beatles;
-                    for(let i = 0; i<band.length;i++){
-                        ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
-                    }
-                }
-                else if(selectedBand=="stones"){
-                    // console.log( JSON.parse(xhr.responseText).stones);  //[{},{},{},{}]
-                    band= JSON.parse(xhr.responseText).stones;
-                    for(let i = 0; i<band.length;i++){
-                        ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
-                    }
-                }
-                else if(selectedBand=="genesis"){
-                    // console.log( JSON.parse(xhr.responseText).genesis);  //[{},{},{},{}]
-                    band= JSON.parse(xhr.responseText).genesis;
-                    for(let i = 0; i<band.length;i++){
-                        ArtistList.innerHTML += "<option>"+band[i].name+"</option>"
-                    }
-                }
-
-                else if(selectedBand=="eagles"){
-                    // console.log( JSON.parse(xhr.responseText).eagles);  //[{},{},{},{}]
-                    band= JSON.parse(xhr.responseText).eagles;
-                    for(let i = 0; i<band.length;i++){
-                        ArtistList.innerHTML += "<option value="+band[i].name+">"+band[i].name+"</option>"
-                    }
-                }
+                // else if(selectedBand=="eagles"){
+                //     // console.log( JSON.parse(xhr.responseText).eagles);  //[{},{},{},{}]
+                //     band= JSON.parse(xhr.responseText).eagles;
+                //     for(let i = 0; i<band.length;i++){
+                //         ArtistList.innerHTML += "<option value="+band[i].name+">"+band[i].name+"</option>"
+                //     }
+                // }
              }
          }
      }
@@ -54,4 +66,5 @@ function getArtists(){
 function openWebsite(){
     var selectedArtist=document.getElementById("Artist").selectedIndex;
     window.location.href=band[selectedArtist].value;
+    //window.open(band[selectedArtist].value); // open in new tab
 }
